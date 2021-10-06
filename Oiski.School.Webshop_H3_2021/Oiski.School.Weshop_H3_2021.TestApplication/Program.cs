90e7859c4c7de5a4490b381764b4747c074b1034
Mike Mortensen <Zhakalen@Gmail.com>
2021-10-06 19:03:36 +0200

    Initial Upload - Added Project Solution - Added Console Application Project   For testing features - Added Datalayer Class Library project   The backend stuff (EF Core)

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Weshop_H3_2021.TestApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oiski.School.Weshop_H3_2021.TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
