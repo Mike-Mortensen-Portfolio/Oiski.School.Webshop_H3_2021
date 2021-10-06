33276a9536b80c35b8ec4d53787ac3245cd69ccb
Mike Mortensen <Zhakalen@Gmail.com>
2021-10-06 21:28:20 +0200

    Implemented Entities - Added  - Customer class  - CustomerLogin class - The login Info for a customer  - Order class  - Product class  - ProductImage class  - Type class - A product type container (Many to Many) - Modified  - README file to include a section for diagrams and added class diagram over entities

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Datalayer/Entities/Customer.cs
@@ -0,0 +1,35 @@
+﻿namespace Oiski.School.Webshop_H3_2021.Datalayer.Entities
+{
+    public class Customer
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int CustomerID { get; set; }
+
+        /// <summary>
+        /// Foreign Key to attached <see cref="CustomerLogin"/>
+        /// </summary>
+        public int? CustomerLoginID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Country { get; set; }
+
+        public int City { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public string Addresse { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int PaymentMethod { get; set; }
+
+        public int DeliveryType { get; set; }
+    }
+}
---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Datalayer/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Oiski.School.Webshop_H3_2021.Datalayer.Entities
+{
+    public class Product
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int ProductID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string BrandName { get; set; }
+
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Collection Navigational Property to the collection of <see cref="ProductImage"/>s attached to the <see cref="Product"/>
+        /// </summary>
+        public ICollection<ProductImage> ProductImages { get; set; }
+
+        public int InStock { get; set; }
+
+        /// <summary>
+        /// Collection Navigational Property to the attached collection of <see cref="Entities.Type"/>s
+        /// </summary>
+        public ICollection<Type> Types { get; set; }
+    }
+}
