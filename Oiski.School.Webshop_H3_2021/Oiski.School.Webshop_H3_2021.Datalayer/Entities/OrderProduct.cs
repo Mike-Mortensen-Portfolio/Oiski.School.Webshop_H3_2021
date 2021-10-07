8fa141955e30886fc6b34b83c1dd55b54a4d281e
Mike Mortensen <mike2137@elevcampus.dk>
2021-10-07 13:06:50 +0200

    Added Nav Properties to OrderProduct

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Datalayer/Entities/OrderProduct.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Datalayer/Entities/OrderProduct.cs
@@ -2,10 +2,27 @@
 
 namespace Oiski.School.Webshop_H3_2021.Datalayer.Entities
 {
-    [Table ("OrderProducts")]
+    [Table("OrderProducts")]
     public class OrderProduct
     {
+        /// <summary>
+        /// Combined Primay Key
+        /// </summary>
         public int ProductID { get; set; }
+
+        /// <summary>
+        /// Reference Navigational Property to the associated <see cref="Entities.Product"/>
+        /// </summary>
+        public Product Product { get; set; }
+
+        /// <summary>
+        /// Combined Primary Key
+        /// </summary>
         public int OrderID { get; set; }
+
+        /// <summary>
+        /// Reference Navigational Property to the associated <see cref="Entities.Order"/>
+        /// </summary>
+        public Order Order { get; set; }
     }
 }
