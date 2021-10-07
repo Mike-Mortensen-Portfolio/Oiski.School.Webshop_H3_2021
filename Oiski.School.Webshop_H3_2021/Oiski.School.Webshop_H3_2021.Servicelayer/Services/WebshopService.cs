594039ccd405e4f4b3eefd8f26446d2cc578affd
Mike Mortensen <Zhakalen@Gmail.com>
2021-10-07 17:18:24 +0200

    - Added   -  Save Changes to Add<T>(T) in WebshopContext     Pushes a new entity to DB   - Update<T>(T) WebshopContext     Updates an existing entity in DB   - Delete<T>(T) in WebshopContext     Deletes an entity from DB   - Find<T>(Func<T, bool>) WebshopContext     Builds an extenable query that targets a specific sequence of type T.

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Servicelayer/Services/WebshopService.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Servicelayer/Services/WebshopService.cs
@@ -1,5 +1,6 @@
 ﻿using Oiski.School.Webshop_H3_2021.Datalayer.Domain;
 using System;
+using System.Linq;
 
 namespace Oiski.School.Webshop_H3_2021.Servicelayer.Services
 {
@@ -36,9 +37,55 @@ namespace Oiski.School.Webshop_H3_2021.Servicelayer.Services
             context = null;
         }
 
+        /// <summary>
+        /// Adds <paramref name="_entity"/> of type <typeparamref name="T"/> to the tracker and saves the changes (<i>Pushes to DB</i>)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_entity">The entity to push to DB</param>
         public void Add<T>(T _entity)
         {
             context.Add(_entity);
+
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Marks <paramref name="_entity"/> of type <typeparamref name="T"/> as <see cref="Microsoft.EntityFrameworkCore.EntityState.Modified"/> and adds it to the tracker, then saves the changes (<i>Pushes to DB</i>)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_entity">The entity to push to DB</param>
+        public void Update<T>(T _entity)
+        {
+            context.Update(_entity);
+
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Adds <paramref name="_entity"/> of type <typeparamref name="T"/> to the tracker as <see cref="Microsoft.EntityFrameworkCore.EntityState.Deleted"/> and saves the changes (<i>Pushes to DB</i>)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_entity">The entity to push to DB</param>
+        public void Remove<T>(T _entity)
+        {
+            context.Remove(_entity);
+
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Builds an <see cref="IQueryable{T}"/> object as an extendable query, based on the <paramref name="_condition"/> provided
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_condition"></param>
+        /// <returns>An extendable query expression that targets a sequence of type <typeparamref name="T"/></returns>
+        public IQueryable<T> Find<T>(Func<T, bool> _condition) where T : class
+        {
+            var query = context.Set<T>("EntityCollection")
+                .Where(_condition)
+                .AsQueryable();
+
+            return query;
         }
     }
 }
