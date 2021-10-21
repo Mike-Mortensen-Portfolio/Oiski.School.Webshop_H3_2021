a70588eeccb3fbe95427b9d90e6d4ff2bd4e93c4
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-10-21 14:09:18 +0200

    Added   - ViewComponent made so that it shows how pieces are InStock on a given Product.

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.H3_2021.Webshop.WebApp/ViewComponents/ProductsInStockViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oiski.School.Webshop_H3_2021.Datalayer.Domain;
+using Oiski.School.Webshop_H3_2021.Servicelayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.H3_2021.Webshop.WebApp.ViewComponents
+{
+    public class ProductsInStockViewComponent : ViewComponent
+    {
+        private readonly WebshopContext _context;
+
+        public ProductsInStockViewComponent(WebshopContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(int productID)
+        {
+            var service = new WebshopService(_context);
+
+            var products = service.GetProductByID(productID);
+
+            return View(products);
+        }
+    }
+}
