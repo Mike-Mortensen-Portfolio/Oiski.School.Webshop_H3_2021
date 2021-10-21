9a814714fc0365258e57b98d0c1354cd49143775
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-10-21 22:39:23 +0200

    Added  - The ability for a customer to create an user, they can Login

--- Oiski.School.Webshop_H3_2021/Oiski.School.H3_2021.Webshop.WebApp/Pages/User/UserCreate.cshtml.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.H3_2021.Webshop.WebApp/Pages/User/UserCreate.cshtml.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Oiski.School.Webshop_H3_2021.Datalayer.Domain;
+using Oiski.School.Webshop_H3_2021.Datalayer.Entities;
+using Oiski.School.Webshop_H3_2021.Servicelayer;
+using Oiski.School.Webshop_H3_2021.Servicelayer.Services;
 
 namespace Oiski.School.H3_2021.Webshop.WebApp.Pages.User
 {
     public class UserCreateModel : PageModel
     {
+        public UserCreateModel(WebshopContext _context)
+        {
+            context = _context;
+        }
+
+        private readonly WebshopContext context;
+
         #region PROPERTIES
         [BindProperty]
         public string FirstNameInput { get; set; }
@@ -29,16 +40,75 @@ namespace Oiski.School.H3_2021.Webshop.WebApp.Pages.User
         [BindProperty]
         public string CityInput { get; set; }
         [BindProperty]
-        public string ZipInput { get; set; }
+        public int ZipInput { get; set; }
+
         #endregion
         public void OnGet()
         {
 
         }
 
-        public void OnPostAddCustomer()
+        public IActionResult OnPostAddCustomer()
         {
+            if (ModelState.IsValid)
+            {
+                var service = new WebshopService(context);
 
+                UserDTO userDTO = service.GetUserByEmail(EmailInput);
+                Customer customer;
+
+                if (userDTO == null)
+                {
+                    customer = new Customer()
+                    {
+                        FirstName = FirstNameInput,
+                        LastName = LastNameInput,
+                        Email = EmailInput,                        
+                        Address = AddressInput,
+                        PhoneNumber = PhoneInput,
+                        Country = CountryInput,
+                        City = CityInput,
+                        ZipCode = ZipInput
+                    };
+                    service.Add(customer);
+
+                    CustomerDTO customerDTO = service.GetUserByEmail(EmailInput);
+
+                    Webshop_H3_2021.Datalayer.Entities.User user = new Webshop_H3_2021.Datalayer.Entities.User()
+                    {
+                        CustomerID = customerDTO.CustomerID,
+                        Password = PasswordInput
+                    };
+
+                    service.Add(user);
+
+                    return RedirectToPage("/Index");
+                }
+
+                customer = new Customer()
+                {
+                    CustomerID = userDTO.CustomerID,
+                    FirstName = userDTO.FirstName,
+                    LastName = userDTO.LastName,
+                    Email = userDTO.Email,
+                    User = new Oiski.School.Webshop_H3_2021.Datalayer.Entities.User()
+                    {
+                        Password = userDTO.Password
+                    },
+                    Address = userDTO.Address,
+                    PhoneNumber = userDTO.PhoneNumber,
+                    Country = userDTO.Country,
+                    City = userDTO.City,
+                    ZipCode = userDTO.ZipCode
+                };
+                service.Update(customer);
+
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                return RedirectToPage("/Error");
+            }
         }
     }
 }
