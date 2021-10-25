facd1f72591bcebc2a2414805ab6a59b463b7abc
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-10-25 12:32:38 +0200

    Added in RestAPI   - Getting all Products   - Getting one Product   - Creating a Product   - Updating a Product   - Deleting a Product

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.RestAPI/Startup.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.RestAPI/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Oiski.School.Webshop_H3_2021.Datalayer.Domain;
+using Oiski.School.Webshop_H3_2021.Servicelayer.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,13 +27,15 @@ namespace Oiski.School.Webshop_H3_2021.RestAPI
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
+        {            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Oiski.School.Webshop_H3_2021.RestAPI", Version = "v1" });
             });
+
+            services.AddDbContext<WebshopContext>();
+            services.AddScoped<IWebshopService, WebshopService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
