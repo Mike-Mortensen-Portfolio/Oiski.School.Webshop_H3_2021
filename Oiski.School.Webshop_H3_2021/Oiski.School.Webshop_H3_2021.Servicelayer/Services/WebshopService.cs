ad2324b319ff57fff6fd2fe03ffab5fb56db81a7
Mike Mortensen <Zhakalen@Gmail.com>
2021-10-27 18:07:52 +0200

    Implemented Service - Added   - IWebshopService   - WebshopService

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Servicelayer/Services/WebshopService.cs
@@ -0,0 +1,22 @@
+﻿using Oiski.School.Webshop_H3_2021.Datalayer.Domain;
+using Oiski.School.Webshop_H3_2021.Servicelayer.Repositories;
+
+namespace Oiski.School.Webshop_H3_2021.Servicelayer.Services
+{
+    public class WebshopService : IWebshopService
+    {
+        public WebshopService()
+        {
+            var context = new WebshopContext();
+            Brand = new BrandRepository(context);
+            Category = new CategoryRepository(context);
+            Customer = new CustomerRepository(context);
+            Product = new ProductRepository(context);
+        }
+
+        public IBrandRepository Brand { get; }
+        public ICategoryRepository Category { get; }
+        public ICustomerRepository Customer { get; }
+        public IProductRepository Product { get; }
+    }
+}
