da50788ad7a50333dfd14f42e712b7f132e5c053
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-10-28 13:35:45 +0200

    Added:   - Unit tests to all of the Methods in our CategoryRepository, all success. Modified  - Added the declaration of GetByIDAsync() in the ICategoryRepository.

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Servicelayer/Repositories/Interfaces/ICategoryRepository.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.Servicelayer/Repositories/Interfaces/ICategoryRepository.cs
@@ -6,5 +6,7 @@ namespace Oiski.School.Webshop_H3_2021.Servicelayer.Repositories
     public interface ICategoryRepository : ICrudRepository<ICategory>
     {
         Task<IReadOnlyList<ICategory>> GetAllAsync();
+
+        Task<ICategory> GetByIDAsync(int _id);
     }
 }
