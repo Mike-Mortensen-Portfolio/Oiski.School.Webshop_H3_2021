25fb8cc5c028e636140d1d91d52b14c1336ccb4c
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-10-31 19:58:43 +0100

    Modified  - Removed the property I earlier added to the `ProductDTO` as this would mean we'd have to change all of our mappings.  - Added a `ProductWithImagesDTO` class where it is deriving from `ProductDTO` but with a `ICollection<ProductImageDTO>` so that we can add a `Product` with `ProductImages` attached to that specific `Product`  - Removed one of the `AddProductAsync()` to only have one, where we now also send a `Product.ProductImages.ToList();`
    This means that our RestAPI is now consistent in form of how the Controllers have been made.

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.RestAPI/Controllers/ProductController.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.RestAPI/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Oiski.School.Webshop_H3_2021.Servicelayer;
 using Oiski.School.Webshop_H3_2021.Servicelayer.Services;
+using Oiski.School.WebShop_H3_2021.RestAPI.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -87,14 +88,7 @@ namespace Oiski.School.WebShop_H3_2021.RestAPI.Controllers
 
         [HttpPost]
         [Route("Products/Create")]
-        public async Task<bool> AddProductAsync(ProductDTO _product)
-        {
-            return await service.Product.AddAsync(_product);
-        }
-
-        [HttpPost]
-        [Route("Products/CreateWithImage")]
-        public async Task<bool> AddProductWithImageAsync(ProductDTO _product)
+        public async Task<bool> AddProductAsync(ProductWithImagesDTO _product)
         {
             return await service.Product.AddAsync(_product, _product.ProductImages.ToList());
         }
---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.RestAPI/DTOs/ProductWithImagesDTO.cs
@@ -0,0 +1,13 @@
+﻿using Oiski.School.Webshop_H3_2021.Servicelayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.WebShop_H3_2021.RestAPI.DTOs
+{
+    public class ProductWithImagesDTO : ProductDTO
+    {
+        public ICollection<ProductImageDTO> ProductImages { get; set; }
+    }
+}
