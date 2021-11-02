316e29b3ec088c8d5e639ea038b99952ea94ebd9
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-11-02 18:45:21 +0100

    Layout for creating a Product.
    I've implemented a layout for the Creating page but, some things have been commented as the method calling our Web API isn't functioning for some reason.
    
     - Added:
       - A new `CreateProduct` page, which will be handling the form of creating a new `Product` in our DB.
       - `BrandDisplay` and `CategoryDisplay` so that we can get the properties in there, and use them when creating a new `Product` as we need this attached to one.
    
    OBS. this is still needing to be finished with functionality.

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/CategoryDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.Webshop_H3_2021.WebUI
+{
+    public class CategoryDisplay
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
