de20cf27bc16b682c298386a551769c70ff3c0e6
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-11-02 15:00:05 +0100

    Uploading work in progress on Login Page.

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/CustomerLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.Webshop_H3_2021.WebUI
+{
+    public class CustomerLogin
+    {
+        public int CustomerID { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
+    }
+}
