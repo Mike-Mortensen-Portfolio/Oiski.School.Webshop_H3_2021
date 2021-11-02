25863757aa4fb1328fdb717e15c8947bcb5ca4fd
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-11-02 13:01:28 +0100

    Layout for displaying a `Product` in a card, to use on the `Index.razor`.
    Added
     - Razor Component where we are using our `ProductDisplay` as a Parameter in our `card`, so that we can use it out in our `Index.razor`
     - A `ProductDisplay` in a folder called `Models`, where we are making a sort of DTO, where we state which properties we need to use from the `Product` entity; to display them.
     - `Images` folder added to the `wwwroot` folder so that we can access our images for each `Product`.

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/ProductDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.Webshop_H3_2021.WebUI.Models
+{
+    public class ProductDisplay
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int BrandID { get; set; }
+        public int CategoryID { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
