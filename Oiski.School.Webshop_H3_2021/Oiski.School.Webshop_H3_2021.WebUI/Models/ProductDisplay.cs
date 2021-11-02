03c6f734e7c7dcc4e9199686cef03bb4762ad88d
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-11-02 13:15:38 +0100

    Removed .Models to the namespace  - Inside of `ProductDisplay` the add-on of `.Models` have been removed from the global namespace as requested. This is done to simplify our Components.  - Removed the unused `@using` in both `Index.razor` and `ProductCard.razor`.

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/ProductDisplay.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/ProductDisplay.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Oiski.School.Webshop_H3_2021.WebUI.Models
+namespace Oiski.School.Webshop_H3_2021.WebUI
 {
     public class ProductDisplay
     {
