608324b51bae7cfc3be667a414f7ae822c341665
Mike Mortensen <mike2137@elevcampus.dk>
2021-11-03 09:10:42 +0100

    Added all base containers for pure API calls
    I've added all the base containers to mach the pure form of JSON recieved from /Controller/[Object]/GetBy/{_[Object]ID}.
    This will make it much easier for us when using the API.
    We can then use interfaces to mask properties or extend the base.
    One example included with the changes is IProductDisplay, which extends the ProductBase to include associated images as URLs.
    - Added
      - BrandBase
      - CategoryBase
      - CustomerBase
      - OrderBase
      - ProductBase
      - IProductDisplay
      - ProductDisplay: Derived from ProductBase and serves as a displayable product where all associated images are attached

---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/BrandBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.Webshop_H3_2021.WebUI
+{
+    /// <summary>
+    /// Serves as the <see langword="base"/> for all brand variation <see langword="objects"/>
+    /// and is the pure form of data recieved and transmitted through the Web API: <strong>Controller/Brands/GetBy/ID/{_brandID}</strong> call.
+    /// </summary>
+    public class BrandBase
+    {
+        public int BrandID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/OrderBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oiski.School.Webshop_H3_2021.WebUI
+{
+    /// <summary>
+    /// Serves as the <see langword="base"/> for all Order variation <see langword="objects"/>
+    /// and is the pure form of data recieved and transmitted through the Web API: <strong>Controller/Orders/GetBy/ID/{_OrderID}</strong> call.
+    /// </summary>
+    public class OrderBase
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public PaymentType TypeOfPayment { get; set; }
+        public DeliveryType TypeOfDelivery { get; set; }
+    }
+
+    public enum PaymentType
+    {
+        Paypal,
+        VISA,
+        Mastercard
+    }
+
+    public enum DeliveryType
+    {
+        [Display(Name = "Post Nord")]
+        PostNord,
+        DAO,
+        GLS
+    }
+}
