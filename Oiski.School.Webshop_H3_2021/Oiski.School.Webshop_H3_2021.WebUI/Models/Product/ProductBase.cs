2c7b478608b83c1a57ecad209f7cecb692186e94
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-11-05 08:22:23 +0100

    Setting required data annotations på ProductBase class.

--- Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/Product/ProductBase.cs
+++ Oiski.School.Webshop_H3_2021/Oiski.School.Webshop_H3_2021.WebUI/Models/Product/ProductBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,11 +13,17 @@ namespace Oiski.School.Webshop_H3_2021.WebUI
     public class ProductBase : IProductBase, ICompactProductInfo
     {
         public int ProductID { get; set; }
+        [Required]
         public int CategoryID { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public int BrandID { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public int InStock { get; set; }
     }
 }
